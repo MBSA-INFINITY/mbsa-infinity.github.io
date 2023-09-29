8f18765f0c0d2a8846e34f9686c35559d0ceb589
M.B.SAI ADITYA <mbsaiaditya@gmail.com>
2023-09-30 01:41:04 +0530

    team pictures updated

---
+++ js/team.js
@@ -0,0 +1,327 @@
+global_team_details = {
+    "2023":`                     <div class="col-lg-12">
+    <div class="team-member" style="border-style: solid; border-width: 0px 0px 3px 0px; border-color: red;">
+      <div class="team-desc">
+        <u>
+          <h3>Dr. SUVIN</h3>
+        </u>
+        <span>
+          <h5 style="color: black;">Faculty Advisor</h5><br>
+        </span>
+        <div class="align-center" style="margin-top : 5%;">
+          <a class="btn btn-s btn-slide btn-light" href="mailto:suvin@nitk.edu.in" data-width="85">
+            <i class="fa fa-envelope"></i>
+            <span>Mail</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-4">
+    <div class="team-member" style="border-style: solid; border-width: 0px 0px 3px 0px; border-color: red;">
+      <div class="team-desc">
+        <img src="../assets/img/empty.webp"  width="150" height="150" style="border-radius: 50%;" />
+        <u>
+          <h3>Aniket Tupe</h3>
+        </u>
+        <span>
+          <h5 style="color: black;">Captain </h5><br>
+        </span>
+        <div class="align-center" style="margin-top : 5%;">
+          <a class="btn btn-s btn-slide btn-light" href="mailto:atupe507@gmail.com" data-width="85">
+            <i class="fa fa-envelope"></i>
+            <span>Mail</span>
+          </a>
+          <a class="btn btn-s btn-slide btn-light" href="https://www.linkedin.com/in/aniket-tupe-859216120/" target="_blank" data-width="115">
+            <i class="fa fa-linkedin"></i>
+            <span>LinkedIn</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-4">
+    <div class="team-member" style="border-style: solid; border-width: 0px 0px 3px 0px; border-color: blue;">
+      <div class="team-desc">
+        <img src="../assets/img/empty.webp"  width="150" height="150" style="border-radius: 50%;" />
+        <u>
+          <h3>Abhilash Bharadwaj</h3>
+        </u>
+        <span>
+          <h5 style="color: black;">Vice Captain - Operations</h5><br>
+        </span>
+        <div class="align-center" style="margin-top : 5%;">
+          <a class="btn btn-s btn-slide btn-light" href="mailto:abhilashbharadwaj.201me227@nitk.edu.in " data-width="85">
+            <i class="fa fa-envelope"></i>
+            <span>Mail</span>
+          </a>
+          <a class="btn btn-s btn-slide btn-light" href="https://www.linkedin.com/in/bharadwaj-848895208	" target="_blank" data-width="115">
+            <i class="fa fa-linkedin"></i>
+            <span>LinkedIn</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-4">
+    <div class="team-member" style="border-style: solid; border-width: 0px 0px 3px 0px; border-color: yellow;">
+      <div class="team-desc">
+        <img src="../assets/img/team/2023/vamshik.png"  width="150" height="150" style="border-radius: 50%;" />
+        <u>
+          <h3>Vamshik M</h3>
+        </u>
+        <span>
+          <h5 style="color: black;">Vice Captain: Technical</h5><br>
+        </span>
+        <div class="align-center" style="margin-top : 5%;">
+          <a class="btn btn-s btn-slide btn-light" href="mailto:vamshikmohan.201me364@nitk.edu.in" data-width="85">
+            <i class="fa fa-envelope"></i>
+            <span>Mail</span>
+          </a>
+          <a class="btn btn-s btn-slide btn-light" href="" target="_blank" data-width="115">
+            <i class="fa fa-linkedin"></i>
+            <span>LinkedIn</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-4">
+    <div class="team-member" style="border-style: solid; border-width: 0px 0px 3px 0px; border-color: blue;">
+      <div class="team-desc">
+         <img src="../assets/img/team/2023/lavanya.png"  width="150" height="150" style="border-radius: 50%;" />
+        <u>
+          <h3>G LAVANYA</h3>
+        </u>
+        <span>
+          <h5 style="color: black;">Design & Analysis Head</h5><br>
+        </span>
+        <div class="align-center" style="margin-top : 5%;">
+          <a class="btn btn-s btn-slide btn-light" href="mailto:lavanya.201me121@nitk.edu.in" data-width="85">
+            <i class="fa fa-envelope"></i>
+            <span>Mail</span>
+          </a>
+          <a class="btn btn-s btn-slide btn-light" href="https://www.linkedin.com/in/lavanya--g" target="_blank" data-width="115">
+            <i class="fa fa-linkedin"></i>
+            <span>LinkedIn</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-4">
+    <div class="team-member" style="border-style: solid; border-width: 0px 0px 3px 0px; border-color: blue;">
+      <div class="team-desc">
+         <img src="../assets/img/team/2023/jeet.png"  width="150" height="150" style="border-radius: 50%;" />
+        <u>
+          <h3>Jeet Vaghasiya</h3>
+        </u>
+        <span>
+          <h5 style="color: black;">Manufacturing Head</h5><br>
+        </span>
+        <div class="align-center" style="margin-top : 5%;">
+          <a class="btn btn-s btn-slide btn-light" href="mailto:vaghasiyajeetpravinbhai.201me162@nitk.edu.in" data-width="85">
+            <i class="fa fa-envelope"></i>
+            <span>Mail</span>
+          </a>
+          <a class="btn btn-s btn-slide btn-light" href="https://www.linkedin.com/in/jeet-vaghasiya-6b0268234" target="_blank" data-width="115">
+            <i class="fa fa-linkedin"></i>
+            <span>LinkedIn</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col-lg-4">
+    <div class="team-member" style="border-style: solid; border-width: 0px 0px 3px 0px; border-color: blue;">
+      <div class="team-desc">
+         <img src="../assets/img/team/2023/goutham.png"  width="150" height="150" style="border-radius: 50%;" />
+        <u>
+          <h3>Goutham V R</h3>
+        </u>
+        <span>
+          <h5 style="color: black;">Convener</h5><br>
+        </span>
+        <div class="align-center" style="margin-top : 5%;">
+          <a class="btn btn-s btn-slide btn-light" href="mailto:gouthamvr.201me221@nitk.edu.in" data-width="85">
+            <i class="fa fa-envelope"></i>
+            <span>Mail</span>
+          </a>
+          <a class="btn btn-s btn-slide btn-light" href="" target="_blank" data-width="115">
+            <i class="fa fa-linkedin"></i>
+            <span>LinkedIn</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div> 
+
+  <div class="col-lg-4">
+    <div class="team-member" style="border-style: solid; border-width: 0px 0px 3px 0px; border-color: blue;">
+      <div class="team-desc">
+         <img src="../assets/img/team/2023/prajjwal.png"  width="150" height="150" style="border-radius: 50%;" />
+        <u>
+          <h3>Prajwal L</h3>
+        </u>
+        <span>
+          <h5 style="color: black;">Media Head</h5><br>
+        </span>
+        <div class="align-center" style="margin-top : 5%;">
+          <a class="btn btn-s btn-slide btn-light" href="mailto:prajwall.211cv136@nitk.edu.in" data-width="85">
+            <i class="fa fa-envelope"></i>
+            <span>Mail</span>
+          </a>
+          <a class="btn btn-s btn-slide btn-light" href="https://www.linkedin.com/in/prajwal-l-754a2022b" target="_blank" data-width="115">
+            <i class="fa fa-linkedin"></i>
+            <span>LinkedIn</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-4">
+    <div class="team-member" style="border-style: solid; border-width: 0px 0px 3px 0px; border-color: blue;">
+      <div class="team-desc">
+         <img src="../assets/img/team/2023/nikhil.png"  width="150" height="150" style="border-radius: 50%;" />
+        <u>
+          <h3>V Nikhil Reddy </h3>
+        </u>
+        <span>
+          <h5 style="color: black;">CAE Head</h5><br>
+        </span>
+        <div class="align-center" style="margin-top : 5%;">
+          <a class="btn btn-s btn-slide btn-light" href="mailto:vangalanikhilreddy.201me261@nitk.edu.in" data-width="85">
+            <i class="fa fa-envelope"></i>
+            <span>Mail</span>
+          </a>
+          <a class="btn btn-s btn-slide btn-light" href="" target="_blank" data-width="115">
+            <i class="fa fa-linkedin"></i>
+            <span>LinkedIn</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-4">
+    <div class="team-member" style="border-style: solid; border-width: 0px 0px 3px 0px; border-color: yellow;">
+      <div class="team-desc">
+         <img src="../assets/img/team/2023/abhimanyu.png"  width="150" height="150" style="border-radius: 50%;" />
+        <u>
+          <h3>Modugula Chinmai </h3>
+        </u>
+        <span>
+          <h5 style="color: black;">VD Team</h5><br>
+        </span>
+        <div class="align-center" style="margin-top : 5%;">
+          <a class="btn btn-s btn-slide btn-light" href="mailto:chinmaiabhimanyu.201me235@nitk.edu.in" data-width="85">
+            <i class="fa fa-envelope"></i>
+            <span>Mail</span>
+          </a>
+          <a class="btn btn-s btn-slide btn-light" href="https://www.linkedin.com/in/chinmai-modugula-5b437b230" target="_blank" data-width="115">
+            <i class="fa fa-linkedin"></i>
+            <span>LinkedIn</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="col-lg-4">
+    <div class="team-member" style="border-style: solid; border-width: 0px 0px 3px 0px; border-color: yellow;">
+      <div class="team-desc">
+         <img src="../assets/img/empty.webp"  width="150" height="150" style="border-radius: 50%;" />
+        <u>
+          <h3>Akshita Jain</h3>
+        </u>
+        <span>
+          <h5 style="color: black;">Content Head</h5><br>
+        </span>
+        <div class="align-center" style="margin-top : 5%;">
+          <a class="btn btn-s btn-slide btn-light" href="mailto:akshita.201me207@nitk.edu.in" data-width="85">
+            <i class="fa fa-envelope"></i>
+            <span>Mail</span>
+          </a>
+          <a class="btn btn-s btn-slide btn-light" href="https://www.linkedin.com/in/akshita-jain-853047203" target="_blank" data-width="115">
+            <i class="fa fa-linkedin"></i>
+            <span>LinkedIn</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-4">
+    <div class="team-member" style="border-style: solid; border-width: 0px 0px 3px 0px; border-color: yellow;">
+      <div class="team-desc">
+         <img src="../assets/img/empty.webp"  width="150" height="150" style="border-radius: 50%;" />
+        <u>
+          <h3>Riya Singh</h3>
+        </u>
+        <span>
+          <h5 style="color: black;">Joint Marketing Head</h5><br>
+        </span>
+        <div class="align-center" style="margin-top : 5%;">
+          <a class="btn btn-s btn-slide btn-light" href="mailto:riyasingh3909@gmail.com" data-width="85">
+            <i class="fa fa-envelope"></i>
+            <span>Mail</span>
+          </a>
+          <a class="btn btn-s btn-slide btn-light" href="https://www.linkedin.com/in/riya-singh28/" target="_blank" data-width="115">
+            <i class="fa fa-linkedin"></i>
+            <span>LinkedIn</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col-lg-4">
+    <div class="team-member" style="border-style: solid; border-width: 0px 0px 3px 0px; border-color: blue;">
+      <div class="team-desc">
+         <img src="../assets/img/empty.webp"  width="150" height="150" style="border-radius: 50%;" />
+        <u>
+          <h3>M.B.Sai Aditya</h3>
+        </u>
+        <span>
+          <h5 style="color: black;">Web Head</h5><br>
+        </span>
+        <div class="align-center" style="margin-top : 5%;">
+          <a class="btn btn-s btn-slide btn-light" href="mailto:mbsaiaditya.201me129@nitk.edu.in" data-width="85">
+            <i class="fa fa-envelope"></i>
+            <span>Mail</span>
+          </a>
+          <a class="btn btn-s btn-slide btn-light" href="" target="_blank" data-width="115">
+            <i class="fa fa-linkedin"></i>
+            <span>LinkedIn</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>`,
+  "2022" :"mbsa"
+}
+
+
+function load_team_details(team_year){
+    local_team_details = global_team_details[team_year]
+    document.getElementById("team_members").innerHTML = local_team_details
+
+}
+
+document.getElementById("year_selection_team").addEventListener('change',()=>{
+    value = document.getElementById("year_selection_team").value
+    load_team_details(value.toString())
+})
+
+load_team_details("2023")
+
+
+
